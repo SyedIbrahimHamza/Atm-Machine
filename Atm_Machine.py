bd14ba07eb6397457d7c05b5996d4eeb62db1d7b
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-08-10 12:37:59 -0700

    Add pin and Transition options

--- Atm_Machine.py
+++ Atm_Machine.py
@@ -1,2 +1,12 @@
 print("Welcome to the ATM Machine!")
 print ("Please enter your PIN to contnue:")
+pin=input()
+if pin=="8965":
+    print("Access granted!")
+    print("Please select an option:")
+    print("1. Check Balance")
+    print("2. Withdraw Money")
+    option=input()
+    while option!=["1","2","3","4"]:
+        print("Invalid option. Please select a valid option:")
+        option=input()

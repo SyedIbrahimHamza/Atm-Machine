5c1c5647c275253a6415238e08c3534be0192c58
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-08-10 13:11:08 -0700

    Add pin limits and lock system

--- Atm_Machine.py
+++ Atm_Machine.py
@@ -1,12 +1,24 @@
 print("Welcome to the ATM Machine!")
 print ("Please enter your PIN to contnue:")
-pin=input()
-if pin=="8965":
-    print("Access granted!")
-    print("Please select an option:")
-    print("1. Check Balance")
-    print("2. Withdraw Money")
+attempts=3
+
+while attempts>0:
+        pin=input()
+        if pin=="9876":
+            break
+        else:
+            attempts-=1
+            if attempts==0:
+                 print("You have entered the wrong PIN 3 times. Your account is now locked.")
+            else:
+                print("Incorrect PIN. You have", attempts, "attempts left.")
+
+
+print("Access granted!")
+print("Please select an option:")
+print("1. Check Balance")
+print("2. Withdraw Money")
+option=input()
+while option not in ["1","2","3","4"]:
+    print("Invalid option. Please select a valid option:")
     option=input()
-    while option!=["1","2","3","4"]:
-        print("Invalid option. Please select a valid option:")
-        option=input()

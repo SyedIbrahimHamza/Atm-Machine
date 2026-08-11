b1d658d252f4c4ca6cb5feac77640e582c3ce7a3
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-08-11 12:52:44 -0700

    Add ATM Features

--- Atm_Machine.py
+++ Atm_Machine.py
@@ -1,24 +1,52 @@
-print("Welcome to the ATM Machine!")
-print ("Please enter your PIN to contnue:")
-attempts=3
+correct_pin = "9876"
+balance = 10000
+attempts = 3
 
-while attempts>0:
-        pin=input()
-        if pin=="9876":
-            break
+print("Welcome to the ATM Machine!")
+print("Please enter your PIN to continue:")
+
+while attempts > 0:
+    pin = input()
+    
+    if pin == correct_pin:
+        break
+    else:
+        attempts -= 1
+        if attempts == 0:
+            print("You have entered the wrong PIN 3 times. Your account is now locked.")
+            exit()  # Program yahin band ho jayega
         else:
-            attempts-=1
-            if attempts==0:
-                 print("You have entered the wrong PIN 3 times. Your account is now locked.")
-            else:
-                print("Incorrect PIN. You have", attempts, "attempts left.")
+            print("Incorrect PIN. You have", attempts, "attempts left.")
 
 
 print("Access granted!")
-print("Please select an option:")
-print("1. Check Balance")
-print("2. Withdraw Money")
-option=input()
-while option not in ["1","2","3","4"]:
-    print("Invalid option. Please select a valid option:")
-    option=input()
+
+
+while True:
+    print("\nPlease select an option:")
+    print("1. Check Balance")
+    print("2. Withdraw Money")
+    print("3. Exit")
+    
+    option = input()
+    
+    
+    while option not in ["1", "2", "3"]:
+        print("Invalid option. Please select a valid option:")
+        option = input()
+    
+    
+    if option == "1":
+        print("Your balance is:", balance)
+    
+    elif option == "2":
+        amount = int(input("Enter amount to withdraw: "))
+        if amount > balance:
+            print("Insufficient balance!")
+        else:
+            balance -= amount
+            print("Withdrawal successful! New balance:", balance)
+    
+    elif option == "3":
+        print("Thank you for using our ATM. Goodbye!")
+        break
